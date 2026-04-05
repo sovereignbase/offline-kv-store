392fce42f94b3a397bedcaa9970f3f1dc25874b1
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-05 21:06:30 +0300

    chore: preparing to make ci connections

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@
  */
 export { KVStore } from './KVStore/class.js'
 export type { Row } from './.types/index.js'
+export type { KVStoreErrorCode } from './.errors/class.js'
 export { resolveDB, destroyDB } from './indexedDB/index.js'
